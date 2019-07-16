3b37d72b5061de6a52c9526cb70865972e72d533
kellel14 <kellel14@gmail.com>
2019-07-16 14:09:48 -0400

    first commit

---
+++ src/main/java/com/example/demo/SpringBoot302Jbcjun10Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot302Jbcjun10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot302Jbcjun10Application.class, args);
+	}
+
+}
